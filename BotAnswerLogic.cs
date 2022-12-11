ff3664956858c4ec2e361b65d9164d1fda4a5c9b
ninas <ninas@LAPTOP-TI0787UD>
2022-12-11 15:47:10 +0300

    Добавлен класс ответа в программу

--- BotAnswerLogic.cs
+++ BotAnswerLogic.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Bot_kot
 {
@@ -18,9 +19,41 @@ namespace Bot_kot
             this.botAnswerDictionary = botAnswerDictionary;
         }
 
-        public async Task ReceiveAsync ( Update update, CancellationToken cancellationToken)
+        public async Task ReceiveAsync(Update update, CancellationToken cancellationToken)
         {
-
+            if (update.Message.Type == MessageType.Sticker)
+            {
+                await telegramBotClient.SendStickerAsync(
+                chatId: update.Message.Chat.Id,
+                sticker: update.Message.Sticker.FileId,
+                cancellationToken: cancellationToken);
+                return;
+            }
+            string answer = "";
+            string textMessage = update.Message.Text.ToLower().Trim();
+            foreach (var value in botAnswerDictionary.Keys)
+            {
+                if (textMessage.Contains(value))
+                {
+                    answer = botAnswerDictionary.GetValueOrDefault(value);
+                    break;
+                };
+            }
+            if (string.IsNullOrEmpty(answer))
+            {
+                await telegramBotClient.SendPhotoAsync(
+                chatId: update.Message.Chat.Id,
+                photo: "https://www.meme-arsenal.com/memes/fa3ede4abb27c0f923e46373adf548b8.jpg",
+                cancellationToken: cancellationToken);
+                return;
+            }
+            else
+            {
+                await telegramBotClient.SendTextMessageAsync(
+                chatId: update.Message.Chat.Id,
+                text: answer,
+                cancellationToken: cancellationToken);
+            }
         }
 
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualBasic;
+﻿using Bot_kot;
+using Microsoft.VisualBasic;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -8,8 +9,14 @@ using Telegram.Bot.Types.Enums;
 var botClient = new TelegramBotClient("5492951142:AAHOqs9esWnt4omwKk47XVHIURiR-Z54AZo");
 
 using CancellationTokenSource cts = new();
+var BotKnowledge = new Dictionary<string, string>()
+    {
+        { "Привет" , "Привет" },
+        { "Как дела" , "Супер" },
+        { "Как ты" , "Все хорошо" }
+    };
 
-
+var TelegramBotLogic = new BotAnswerLogic(botClient, BotKnowledge);
 // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
 ReceiverOptions receiverOptions = new()
 {
@@ -33,55 +40,8 @@ cts.Cancel();
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
-    if (update.Message.Type == MessageType.Sticker)
-    {
-        await botClient.SendStickerAsync(
-        chatId: update.Message.Chat.Id,
-        sticker: update.Message.Sticker.FileId,
-        cancellationToken: cancellationToken);
-        return;
-    }
-
-    // Only process Message updates: https://core.telegram.org/bots/api#message
-    if (update.Message is not { } message)
-        return;
-    // Only process text messages
-    if (message.Text is not { } messageText)
-        return;
-
-    var chatId = message.Chat.Id;
-
-    Console.WriteLine($"Received a '{message.Text}' message in chat {chatId}.");
-
-    var people = new Dictionary<string, string>()
-    {
-        { "Привет" , "Привет" },
-        { "Как дела" , "Супер" },
-        { "Как ты" , "Все хорошо" }
-    };
-
-    string messageAnswerText = "";
-    switch (message.Text)
-    {
-        case "Привет":
-            messageAnswerText = "Привет";
-            break;
-        case "Как дела?":
-            messageAnswerText = "Супер, а у тебя?";
-            break;
-        default:
-            await botClient.SendPhotoAsync(
-                chatId: chatId,
-                photo: "https://www.meme-arsenal.com/memes/fa3ede4abb27c0f923e46373adf548b8.jpg",
-                cancellationToken: cancellationToken);
-            return;
-    }
-
-    // Echo received message text
-    Message sentMessage = await botClient.SendTextMessageAsync(
-        chatId: chatId,
-        text: messageAnswerText,
-        cancellationToken: cancellationToken);
+    Console.WriteLine($"Received a '{update.Message.Text}' message in chat {update.Message.Chat.Id}.");
+    await TelegramBotLogic.ReceiveAsync( update, cancellationToken);
 }
 
 Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
